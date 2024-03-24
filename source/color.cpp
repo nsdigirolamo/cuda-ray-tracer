7c63919c8e98dd3c2109fac995637469dad9d114
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-23 20:07:08 -0400

    Added color class.

---
+++ source/color.cpp
@@ -0,0 +1,59 @@
+#include "color.hpp"
+
+Color::Color (const Vector<3>& vector) {
+    for (int i = 0; i < 3; ++i) {
+        (*this)[i] = vector[i];
+    }
+}
+
+Color::Color () {
+    Vector<3> vector;
+    *this = { vector };
+}
+
+Color::Color (const double (&values)[3]) {
+    Vector<3> vector { values };
+    *this = { vector };
+}
+
+Color::Color (const double red, const double green, const double blue) {
+    Vector<3> vector {{ red, green, blue }};
+    *this = { vector };
+}
+
+Color::Color (const int rgb_hex_value) {
+
+    int val = rgb_hex_value < MIN_HEX ? 
+        MIN_HEX : MAX_HEX < rgb_hex_value ? 
+            MAX_HEX : rgb_hex_value;
+
+    double b = val % 0x100;
+    double g = (val >> 8) % 0x100;
+    double r = (val >> 16);
+
+    *this = { r / 256, g / 256, b / 256 };
+}
+
+const double& Color::r () const {
+    return (*this)[0];
+}
+
+const double& Color::g () const {
+    return (*this)[1];
+}
+
+const double& Color::b () const { 
+    return (*this)[2];
+}
+
+double& Color::r () {
+    return (*this)[0];
+}
+
+double& Color::g () {
+    return (*this)[1];
+}
+
+double& Color::b () { 
+    return (*this)[2];
+}
