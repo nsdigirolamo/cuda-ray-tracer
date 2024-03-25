83bbb7c89c5c0a18586f7183f4072849ff572fd2
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-24 20:32:58 -0400

    Added constructors.

--- include/hit.hpp
+++ include/hit.hpp
@@ -8,14 +8,21 @@ class Hit {
 
     public:
 
-        const bool exists; // True if the hit exists.
-
         const Ray incoming; // The ray that caused the hit.
         const double distance; // The distance along the incoming ray where the hit occurred.
 
         const Point origin; // The location of the hit in the scene.
         const UnitVector<3> surface_normal; // The normal of the surface that was hit.
         const bool is_front; // True if the hit occurred on the front of the surface.
+
+        Hit ();
+        Hit (
+            const Ray& incoming,
+            const double distance,
+            const Point& origin,
+            const UnitVector<3>& surface_normal,
+            const bool is_front
+        );
 };
 
 #endif
