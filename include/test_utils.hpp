921ed8661c176b3bd3d890ef236b4fd64404b0be
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-24 22:09:06 -0400

    Added testing macros for rays, hits, and optional hits.

--- include/test_utils.hpp
+++ include/test_utils.hpp
@@ -9,6 +9,21 @@
     for (int i = 0; i < lhs.height(); ++i) { \
         CAPTURE(i); \
         CHECK(lhs[i] == doctest::Approx(rhs[i])); \
-    } \
+    }
+
+#define CHECK_RAYS(lhs, rhs) \
+    CHECK_VECTORS(lhs.origin, rhs.origin); \
+    CHECK_VECTORS(lhs.direction, rhs.direction);
+
+#define CHECK_HITS(lhs, rhs) \
+    CHECK_RAYS(lhs.incoming, rhs.incoming); \
+    CHECK(lhs.distance == doctest::Approx(rhs.distance)); \
+    CHECK_VECTORS(lhs.origin, rhs.origin); \
+    CHECK_VECTORS(lhs.surface_normal, rhs.surface_normal); \
+    CHECK(lhs.is_front == rhs.is_front);
+
+#define CHECK_OPTIONAL_HITS(lhs, rhs) \
+    REQUIRE(lhs.exists == rhs.exists); \
+    if (lhs.exists) CHECK_HITS(lhs, rhs);
 
 #endif
