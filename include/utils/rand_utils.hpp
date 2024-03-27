beae71ce4206a268acbf8ff501607f1a94cfb4b4
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-27 18:52:25 -0400

    Added more random utils.

--- include/utils/rand_utils.hpp
+++ include/utils/rand_utils.hpp
@@ -5,7 +5,18 @@
 
 #include "vector.hpp"
 
+#define RANDOM_DOUBLE(state) 2 * curand_uniform_double(state) - 1
+
 __device__ Point randomInUnitSphere (curandState* state);
+
 __device__ Point randomOnUnitSphere (curandState* state);
+
+__device__ Vector<3> randomInUnitHemisphere (const UnitVector<3>& normal, curandState* state);
+
+__device__ UnitVector<3> randomOnUnitHemisphere (const UnitVector<3>& normal, curandState* state);
+
+__device__ Vector<2> randomInUnitCircle (curandState* state);
+
+__device__ UnitVector<2> randomOnUnitCircle (curandState* state);
 
 #endif
--- source/utils/rand_utils.cu
+++ source/utils/rand_utils.cu
@@ -7,9 +7,9 @@ __device__ Point randomInUnitSphere (curandState* state) {
 
     do {
 
-        x = curand_uniform_double(state);
-        y = curand_uniform_double(state);
-        z = curand_uniform_double(state);
+        x = RANDOM_DOUBLE(state);
+        y = RANDOM_DOUBLE(state);
+        z = RANDOM_DOUBLE(state);
 
         p = {{ x, y, z }};
 
@@ -21,4 +21,37 @@ __device__ Point randomInUnitSphere (curandState* state) {
 __device__ Point randomOnUnitSphere (curandState* state) {
 
     return (UnitVector<3>)(randomInUnitSphere(state));
+}
+
+__device__ Vector<3> randomInUnitHemisphere (const UnitVector<3>& normal, curandState* state) {
+
+    Point in_sphere = randomInUnitSphere(state);
+    return 0 < dot(normal, in_sphere) ? in_sphere : -1.0 * in_sphere;
+}
+
+__device__ UnitVector<3> randomOnUnitHemisphere (const UnitVector<3>& normal, curandState* state) {
+
+    return (UnitVector<3>)(randomInUnitHemisphere(normal, state));
+}
+
+__device__ Vector<2> randomInUnitCircle (curandState* state) {
+
+    double x, y;
+    Vector<2> p;
+
+    do {
+
+        x = RANDOM_DOUBLE(state);
+        y = RANDOM_DOUBLE(state);
+
+        p = {{ x, y }};
+
+    } while (1 < p.length());
+
+    return p;
+}
+
+__device__ UnitVector<2> randomOnUnitCircle (curandState* state) {
+
+    return (UnitVector<2>)(randomInUnitCircle(state));
 }
