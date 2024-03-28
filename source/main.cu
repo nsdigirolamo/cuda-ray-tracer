3fb62394721a65388cd041d7d388de64a5849b29
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-27 22:18:38 -0400

    Debugging depth of field.

--- source/main.cu
+++ source/main.cu
@@ -9,12 +9,17 @@ int main () {
     clock_t start = clock();
 
     Camera camera {
-        {{ 0, 0, -5 }},
+        {{ 0, 0, 0 }},
         1080,
         1920,
         90,
-        {{ 0, 0, 0 }}
+        {{ 0, 0, 20 }},
+        5.0
     };
+
+    std::cout << camera.getLensRadius() << "\n";
+    std::cout << camera.getFocalDistance() << "\n";
+    std::cout << camera.getViewDirection()[0] << " " << camera.getViewDirection()[1] << " " << camera.getViewDirection()[2] << "\n";
 
     Image image = camera.render(50, 50);
 
