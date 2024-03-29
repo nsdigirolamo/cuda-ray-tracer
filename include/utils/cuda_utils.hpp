32f49fa4911239e11c6be707abb113a922354562
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-29 15:39:25 -0400

    Added missing includes guard.

--- include/utils/cuda_utils.hpp
+++ include/utils/cuda_utils.hpp
@@ -1,3 +1,8 @@
+#ifndef CUDA_UTILS_HPP
+#define CUDA_UTILS_HPP
+
 #include "stdio.h"
 
 #define CUDA_ERROR_CHECK(error) if (error != cudaSuccess) { fprintf(stderr, "%s %i %s: %s.\n", __FILE__, __LINE__, cudaGetErrorName(error), cudaGetErrorString(error)); exit(error); }
+
+#endif
