363cac0e1a8d7c4cc6709e38ddf711f8eada30bb
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-31 21:13:51 -0400

    Changes to hit detection code to pass test cases.

--- source/aabb.cu
+++ source/aabb.cu
@@ -1,5 +1,7 @@
 #include "aabb.hpp"
 
+#include "stdio.h"
+
 __host__ __device__ AABB::AABB (
     const Hittable* bounded,
     const Interval& x_interval,
@@ -25,14 +27,24 @@ __host__ __device__ bool AABB::isHit (const Ray& ray) const {
 
     for (int i = 0; i < 3; ++i) {
 
-        double min_calc = ray.origin[i] - this->getInterval(i).min / ray.direction[i];
-        double max_calc = ray.origin[i] - this->getInterval(i).max / ray.direction[i];
+        double min_calc = (this->getInterval(i).min - ray.origin[i]) / ray.direction[i];
+        double max_calc = (this->getInterval(i).max - ray.origin[i]) / ray.direction[i];
+
+        if (isnan(min_calc)) min_calc = -1.0 * max_calc;
+        if (isnan(max_calc)) max_calc = -1.0 * min_calc;
 
         intervals[i] = { min_calc, max_calc };
     }
 
-    return
+    bool interval_behind_ray =
+        (intervals[0].min < 0 && intervals[0].max < 0) ||
+        (intervals[1].min < 0 && intervals[1].max < 0) ||
+        (intervals[2].min < 0 && intervals[2].max < 0);
+
+    bool all_overlap =
         IS_OVERLAPPING(intervals[0], intervals[1]) &&
         IS_OVERLAPPING(intervals[1], intervals[2]) &&
         IS_OVERLAPPING(intervals[2], intervals[0]);
+
+    return all_overlap && !interval_behind_ray;
 }
